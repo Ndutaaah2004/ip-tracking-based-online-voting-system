dd67ef68a0e39b97636dd1e075c4a403cfcf6e72
Ndutaaah2004 <ndutaahwainaina@gmail.com>
2026-03-30 23:22:02 +0300

    update ingress

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = getenv('DB_HOST') ?: 'localhost';
-$user = getenv('DB_USER') ?: 'root';
-$password = getenv('DB_PASSWORD') !== false ? getenv('DB_PASSWORD') : '';
+$host = getenv('DB_HOST') ?: 'private-db-mysql-ams3-57287-do-user-23404159-0.k.db.ondigitalocean.com';
+$user = getenv('DB_USER') ?: 'app_user';
+$password = getenv('DB_PASSWORD') !== false ? getenv('DB_PASSWORD') : 'StrongPassword123!';
 $database = getenv('DB_NAME') ?: 'voting_system';
 
 $conn = new mysqli($host, $user, $password, $database);
