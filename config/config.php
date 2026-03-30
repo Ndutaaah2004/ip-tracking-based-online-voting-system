0565151e0e83371887fb1d29d43c911b795d8bb3
Ndutaaah2004 <ndutaahwainaina@gmail.com>
2026-03-30 23:28:36 +0300

    update ingress

--- config/config.php
+++ config/config.php
@@ -4,8 +4,9 @@ $host = getenv('DB_HOST') ?: 'private-db-mysql-ams3-57287-do-user-23404159-0.k.d
 $user = getenv('DB_USER') ?: 'app_user';
 $password = getenv('DB_PASSWORD') !== false ? getenv('DB_PASSWORD') : 'StrongPassword123!';
 $database = getenv('DB_NAME') ?: 'voting_system';
+$port = 25060;
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database,$port);
 
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
